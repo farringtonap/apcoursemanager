76f8f2de7db10eb7b361caaef29f5a051d6ed589
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-13 22:31:33 -1000

    Minor Changes: Added link to source code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,29 @@
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { BoxArrowUpRight, Github } from 'react-bootstrap-icons';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3" style={{ backgroundColor: '#ffffff' }}>
-    <Container>
-      <Col className="text-center">
-        Created by Levi Kuhaulua, Ivan Ramel, Harrison Law, James Yamada, Deavyn Etscheit
-        {' '}
-        <a href="https://github.com/farringtonap/farrington-ap.github.io">Github Page</a>
-      </Col>
+    <Container fluid className="text-center border-top">
+      <Row className="mt-2">
+        <Col>
+          Created by Levi Kuhaulua, Ivan Ramel, Harrison Law, James Yamada, & Deavyn Etscheit
+        </Col>
+      </Row>
+      <Row xs="auto" className="justify-content-center mt-2">
+        <Col className="d-inline-flex justify-content-center align-items-center gap-2">
+          <BoxArrowUpRight />
+          <a href="https://farringtonap.github.io/farrington-ap.github.io/">
+            Github Page
+          </a>
+        </Col>
+        <Col className="d-inline-flex justify-content-center align-items-center gap-2">
+          <Github />
+          <a href="https://github.com/farringtonap/apcoursemanager">
+            Source Code
+          </a>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
