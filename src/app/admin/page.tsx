f8f23a2b8d5555d15d83697d3cfbfebddcf5e0f3
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-18 12:36:43 -1000

    Added: General Layout for displaying users on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,19 @@
 import { getServerSession } from 'next-auth';
-import { Col, Container, Table } from 'react-bootstrap';
+import
+{ Card,
+  Container,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Row,
+  Col,
+  CardFooter,
+  CardText } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
+import { PencilSquare, XSquare } from 'react-bootstrap-icons';
 
 const AdminPage = async () => {
   const session = await getServerSession(authOptions);
@@ -15,26 +26,40 @@ const AdminPage = async () => {
 
   return (
     <main>
-      <Container id="list" fluid className="py-3">
-        <Col>
-          <h1>Registered Users</h1>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Email</th>
-                <th>Role</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.email}</td>
-                  <td>{user.role}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Col>
+      <Container id="list" className="py-3 mx-auto">
+        <Row lg={3} className="g-4">
+          {users.map((user) => (
+            <Col sm key={user.email}>
+              <Card>
+                <CardBody>
+                  <CardTitle className="h3 fw-bold">{`${user.firstName} ${user.lastName}`}</CardTitle>
+                  <CardSubtitle className="small text-muted mb-4">{user.email}</CardSubtitle>
+                  <CardText />
+                  <CardFooter className="d-flex flex-row justify-content-start align-items-center ps-0 gap-2">
+                    <Button
+                      variant="outline-primary"
+                      href="/"
+                      size="sm"
+                      className="d-flex justify-content-center align-items-center gap-1 fw-medium"
+                    >
+                      <PencilSquare />
+                      Edit
+                    </Button>
+                    <Button
+                      variant="outline-danger"
+                      href="/"
+                      size="sm"
+                      className="d-flex justify-content-center align-items-center gap-1 fw-medium"
+                    >
+                      <XSquare />
+                      Delete
+                    </Button>
+                  </CardFooter>
+                </CardBody>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </main>
   );
