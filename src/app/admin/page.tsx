e21cec169f1950fde9d3dfc3f44f1edd459a6f90
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-04-27 21:22:09 -1000

    Refactor: Added white bg to members page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -21,48 +21,50 @@ const AdminPage = async () => {
     <main className="mx-auto">
       <Container id="list" className="py-3">
         <Button
-          href="/"
+          href="/admin/adduser"
           variant="success"
           className="p-2 text-white fw-medium d-flex justify-content-center align-items-center gap-2 mb-4"
           style={{ width: 'fit-content' }}
         >
           <PlusCircle size={20} />
-          Add Member
+          Add User
         </Button>
         <Row lg={3} className="g-5">
           {users.map((user) => (
             <Col sm key={user.email} className="mb-2">
-              <hgroup>
-                <p
-                  className="text-capitalize px-2 bg-danger text-white small rounded-pill"
-                  style={{ width: 'fit-content' }}
-                >
-                  {`${user.role.toLowerCase()}`}
-                </p>
-                <h3 className="h3 text-black fw-bold lh-1">{`${user.firstName} ${user.lastName}`}</h3>
-                <p className="small text-muted">
-                  {`${user.email}`}
-                </p>
-              </hgroup>
-              <div className="d-flex flex-row gap-2">
-                <Button
-                  variant="outline-primary"
-                  href={`admin/edit-user/${user.id}`}
-                  size="sm"
-                  className="d-flex justify-content-center align-items-center gap-1 fw-medium"
-                >
-                  <PencilSquare />
-                  Edit
-                </Button>
-                <Button
-                  variant="outline-danger"
-                  href="/"
-                  size="sm"
-                  className="d-flex justify-content-center align-items-center gap-1 fw-medium"
-                >
-                  <XSquare />
-                  Delete
-                </Button>
+              <div className="py-3 px-2 bg-white shadow-sm rounded-2">
+                <hgroup>
+                  <p
+                    className="text-capitalize px-2 bg-danger text-white small rounded-pill"
+                    style={{ width: 'fit-content' }}
+                  >
+                    {`${user.role.toLowerCase()}`}
+                  </p>
+                  <h3 className="h3 text-black fw-bold lh-1">{`${user.firstName} ${user.lastName}`}</h3>
+                  <p className="small text-muted">
+                    {`${user.email}`}
+                  </p>
+                </hgroup>
+                <div className="d-flex flex-row gap-2">
+                  <Button
+                    variant="outline-primary"
+                    href={`admin/edituser/${user.id}`}
+                    size="sm"
+                    className="d-flex justify-content-center align-items-center gap-1 fw-medium"
+                  >
+                    <PencilSquare />
+                    Edit
+                  </Button>
+                  <Button
+                    variant="outline-danger"
+                    href="/"
+                    size="sm"
+                    className="d-flex justify-content-center align-items-center gap-1 fw-medium"
+                  >
+                    <XSquare />
+                    Delete
+                  </Button>
+                </div>
               </div>
             </Col>
           ))}
