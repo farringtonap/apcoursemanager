de0a224968a7dda2aabea8439b916e1ad14f9c8f
jameshakuy <jameshy@hawaii.edu>
2025-05-03 19:28:46 -1000

    linr

--- src/app/recommendation/page.tsx
+++ src/app/recommendation/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-
 import { useState, FormEvent } from 'react';
-import { Form, Button, Alert, Row, Col } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 const PYTHON_API_URL = process.env.NEXT_PUBLIC_PYTHON_API_URL || 'http://localhost:8000';
 
