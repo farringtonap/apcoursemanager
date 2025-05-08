8eed425e0d2626fdbe0f755b79e4e7188de83e4f
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-05-07 21:36:32 -1000

    Refactor Edit User form

--- src/app/admin/edituser/[id]/page.tsx
+++ src/app/admin/edituser/[id]/page.tsx
@@ -1,7 +1,9 @@
 import { getServerSession } from 'next-auth';
 import authOptions from '@/lib/authOptions';
 import { adminProtectedPage } from '@/lib/page-protection';
+import { prisma } from '@/lib/prisma';
 import EditUserForm from '@/components/EditUserForm';
+import NotFound from '@/app/not-found';
 
 const EditUser = async ({ params } : { params: { id: string } }) => {
   // Redirects user to appropriate link if not an admin or not signed in
@@ -12,11 +14,15 @@ const EditUser = async ({ params } : { params: { id: string } }) => {
     } | null,
   );
 
-  const { id } = await params;
+  const id = Number(await params.id);
+  const user = await prisma.user.findUnique({ where: { id } });
 
+  if (!user) {
+    return NotFound();
+  }
   return (
     <main>
-      <EditUserForm id={id} />
+      <EditUserForm user={user} />
     </main>
   );
 };
--- src/components/EditUserForm.tsx
+++ src/components/EditUserForm.tsx
@@ -1,23 +1,68 @@
+'use client';
+
 import { User } from '@prisma/client';
-import NotFound from '@/app/not-found';
-import { prisma } from '@/lib/prisma';
+import {
+  Container,
+  Form,
+  FormControl,
+  Row,
+  FormSelect,
+  FormLabel,
+  FormGroup,
+  Button,
+  Col,
+  ButtonGroup,
+} from 'react-bootstrap';
 
-export default async function EditUserForm({ id } : { id:String }) {
-  const uid: User | null = await prisma.user.findUnique({ where: { id: Number(id) } });
-
-  if (!uid) {
-    return NotFound();
-  }
-
+export default function EditUserForm({ user }: { user: User }) {
   return (
-    <h1>
-      This is
-      {' '}
-      {uid.email}
-      {' '}
-      {uid.id}
-      {' '}
-      User
-    </h1>
+    <Container fluid className="fw-bold justify-content-center align-items-center">
+      <Row className="justify-content-center align-items-center">
+        <Col xs={5} className="mx-auto bg-white text-black p-4 mt-3 rounded-2">
+          <h1 className="h1">Update Account</h1>
+          <Form>
+            <input type="hidden" value={user.id} />
+            <FormGroup>
+              <FormLabel htmlFor="first-name">First Name</FormLabel>
+              <FormControl
+                id="first-Name"
+                type="text"
+                defaultValue={user.firstName}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel htmlFor="last-name">Last Name</FormLabel>
+              <FormControl
+                id="last-name"
+                type="text"
+                defaultValue={user.lastName}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <FormControl
+                id="password"
+                type="password"
+                required
+                placeholder="Change password here..."
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel htmlFor="role" sm>Role</FormLabel>
+              <FormSelect required>
+                <option value="TEACHER" selected={user.role === 'TEACHER'}>Teacher</option>
+                <option value="ADMIN" selected={user.role === 'ADMIN'}>Admin</option>
+              </FormSelect>
+            </FormGroup>
+            <ButtonGroup className="mt-4">
+              <Button href="/admin/" variant="warning">Back</Button>
+              <Button variant="primary">Submit</Button>
+            </ButtonGroup>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
