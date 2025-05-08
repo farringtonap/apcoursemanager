c0ad684e59cefced0f0375ee521988acfd1e7e82
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2025-05-07 21:36:12 -1000

    Prisma Migrations

--- prisma/migrations/20250428051738_authorized_user/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "AuthorizedUser" (
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "AuthorizedUser_pkey" PRIMARY KEY ("email")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "AuthorizedUser_email_key" ON "AuthorizedUser"("email");
--- prisma/migrations/20250428064131_authorized_user_role/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "AuthorizedUser" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'TEACHER';
